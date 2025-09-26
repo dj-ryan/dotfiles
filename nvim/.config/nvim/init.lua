f8d840f7bb382d1baa83e3ad67cf409b1cb6f1d7
daryan <daryan@amazon.com>
2025-09-26 16:16:03 +0000

    feat: changed to spaces instead of tabs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -54,6 +54,12 @@ vim.o.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
 
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+
+-- use two spaces for a tab
+vim.o.tabstop = 2
+
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 vim.o.confirm = true
